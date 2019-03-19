6beb7a3308a40bee1ac033f3bd85942b0018afc2
DaryaBobko <bobko.dv@gmail.com>
2019-03-19 15:31:27 +0300

    Build forder added for deploy

---
+++ ctask/build/precache-manifest.be211485d5d5a0d75aa7105b9a0cb0b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9a3322a389b6a97e4132a355c813dc7",
+    "url": "/static/media/bear-face.c9a3322a.svg"
+  },
+  {
+    "revision": "c487c7ea92b2815199a7",
+    "url": "/static/css/main.67025f91.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1d9b28826f9e725c4a5c",
+    "url": "/static/js/2.52fe987f.chunk.js"
+  },
+  {
+    "revision": "f0b7b098d2d04b5e341087c0c34a37dd",
+    "url": "/static/media/logo.f0b7b098.svg"
+  },
+  {
+    "revision": "c487c7ea92b2815199a7",
+    "url": "/static/js/main.274ada8f.chunk.js"
+  },
+  {
+    "revision": "5995bae2cbb96f35afbe21e049b2f644",
+    "url": "/static/media/bear-face-purpur.5995bae2.svg"
+  },
+  {
+    "revision": "b575c000bd609cf4aa78c24f34d82a8c",
+    "url": "/static/media/icon.b575c000.svg"
+  },
+  {
+    "revision": "690669248912a76bfdd9755849bf5014",
+    "url": "/static/media/add.69066924.svg"
+  },
+  {
+    "revision": "834b474def57307ad688190e675e8fee",
+    "url": "/static/media/cancel.834b474d.svg"
+  },
+  {
+    "revision": "6ce2394e81cb5ec682852143d1d15536",
+    "url": "/index.html"
+  }
+];
